373b2efde2c71e1cbe1644cfdbacf4e0e1336705
sekrit-twc <noreply@example.com>
2018-01-07 10:46:59 -0800

    Use unbuffered I/O for slave log file.

--- avshost_native/main.cpp
+++ avshost_native/main.cpp
@@ -42,8 +42,10 @@ class Session : ipc_client::CommandObserver {
 		if (s_log_file)
 			return 0;
 
-		if ((s_log_file = _wfopen(c->arg().c_str(), L"w")))
+		if ((s_log_file = _wfopen(c->arg().c_str(), L"w"))) {
+			std::setvbuf(s_log_file, nullptr, _IONBF, 0);
 			ipc_set_log_handler(log_to_file, log_to_file);
+		}
 
 		return 0;
 	}
